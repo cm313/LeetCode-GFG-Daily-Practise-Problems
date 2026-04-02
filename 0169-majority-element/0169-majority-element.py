afcee6f4e4cb8e2db5948e127217be96ea35df9e
Chanda Chandra Mahesh <93776284+cm313@users.noreply.github.com>
2026-04-02 23:57:57 +0530

    Time: 3 ms (86.11%), Space: 21.3 MB (35.43%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -10,21 +10,5 @@ class Solution:
         #     if value > n/2:
         #         return key
         nums.sort()
-        count = 0 
-        i, j = 0,0
         n = len(nums)
-        if n == 1:
-            return nums[0]
-        while (j<n-1):
-            if nums[j] == nums[j+1]:
-                count += 1
-                j += 1
-            else:
-                if count+1 > n/2:
-                    return nums[j]
-                else:
-                    count = 0
-                    j = j+1
-                    i = j   
-        if count+1 > n/2:
-            return nums[j]                 
+        return nums[int(n/2)]               
