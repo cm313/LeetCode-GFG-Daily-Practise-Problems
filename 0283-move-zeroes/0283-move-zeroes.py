79e53846b57393a0443b5377b97abb50cbedc812
Chanda Chandra Mahesh <93776284+cm313@users.noreply.github.com>
2026-04-15 23:53:58 +0530

    Time: 4 ms (60.27%), Space: 20.5 MB (34.02%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,11 +3,12 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        slow = 0
-        for fast in range(len(nums)):
-            if nums[fast] != 0:
-                nums[slow], nums[fast] = nums[fast], nums[slow]
-                slow += 1
-
-
-                    
+        n = len(nums)
+        i,j=0,0
+        while(j<n):
+            if  nums[i]==0 and nums[j] !=0 :
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+            elif nums[i]!=0 and nums[j]!=0:
+                i += 1
+            j += 1    
