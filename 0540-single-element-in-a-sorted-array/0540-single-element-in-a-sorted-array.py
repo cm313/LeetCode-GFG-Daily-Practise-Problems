27c2a5e6435bf52e54815cce5e4b08c7d9ee53e2
Chanda Chandra Mahesh <93776284+cm313@users.noreply.github.com>
2026-06-10 22:11:47 +0530

    Time: 0 ms (100%), Space: 27.1 MB (11.34%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -1,11 +1,39 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        thisDict:dict = {}
-        for i in range(len(nums)):
-            if nums[i] in thisDict:
-                thisDict[nums[i]] = thisDict[nums[i]]+1
-            else: 
-                thisDict[nums[i]] = 1
-        for i,j in thisDict.items():
-            if j == 1:
-                return i        
+        n = len(nums)
+        s,e = 0, n-1
+        ans = -1
+        if n == 1:
+            return nums[0]
+        while(s <= e):
+            mid = s + (e-s)//2
+            if mid == 0:
+                if nums[mid] == nums[mid+1]:
+                    s = mid+2
+                else:
+                    ans = nums[mid]
+                    break
+            elif mid == n-1:
+                if nums[mid] == nums[mid-1]:
+                    e = mid-2
+                else:
+                    ans = nums[mid]  
+                    break        
+            else:
+                if mid%2 == 0:
+                    if nums[mid] == nums[mid-1]:
+                        e  = mid-2
+                    elif nums[mid] == nums[mid+1]:
+                        s = mid+2    
+                    else:
+                        ans = nums[mid]   
+                        break
+                else:
+                    if nums[mid] == nums[mid-1]:
+                        s = mid+1
+                    elif nums[mid] == nums[mid+1]:
+                        e = mid-1 
+                    else:
+                        ans = nums[mid]  
+                        break         
+        return ans    
