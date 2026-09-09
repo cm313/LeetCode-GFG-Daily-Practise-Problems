2fff124b16f14834fd5ade937f3f3ec85095db64
Chanda Chandra Mahesh <93776284+cm313@users.noreply.github.com>
2026-09-09 22:10:28 +0530

    Time: 4319 ms (5.07%), Space: 20.3 MB (99.97%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,9 +1,5 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        dict = {}
-        for i in range(len(nums)):
-            val = dict.setdefault(nums[i],0) + 1
-            dict[nums[i]] = val
-        for key, val in  dict.items():
-            if val == 1:
-                return key       
+        for val in nums:
+            if nums.count(val) == 1:
+                return val
